07823099ea36f224fbd89cf02868ba12441d8566
Henar <henardw01@gmail.com>
2026-02-06 17:25:46 +0100

    previous file
    -

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    updateClockNav();                   
-    setInterval(updateClockNav, 1000); 
+    updateClockNav();
+    setInterval(updateClockNav, 1000);
 
     const addBtn = document.getElementById('addBtn');
     const taskInput = document.getElementById('taskInput');
     const columns = document.querySelectorAll('.column');
-    
+    const filterBtn = document.getElementById('filterBtn');
+    const filterInput = document.getElementById('filterSearch');
+
 
     // Add task on button click
     addBtn.addEventListener('click', createTask);
@@ -17,10 +19,27 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // Show search input when click on button
+    filterBtn.addEventListener('click', () => {
+        filterBtn.classList.add('hidden');
+        filterInput.classList.remove('hidden');
+        filterInput.focus(); 
+    });
+
+    // goes back to being a button
+    filterInput.addEventListener('blur', () => {
+        if (filterInput.value.trim() === "") {
+            filterInput.classList.add('hidden');
+            filterBtn.classList.remove('hidden');
+        }
+    });
+
+    filterInput.addEventListener('input', filterTasks, false);
+
     // Initialize columns with drag & drop events
     columns.forEach(column => {
         const taskList = column.querySelector('.task-list');
-        
+
         column.addEventListener('dragover', (e) => {
             e.preventDefault();
             column.classList.add('drag-over');
@@ -72,35 +91,35 @@ function createTask(text) {
     const taskContent = document.createElement('p');
     taskContent.textContent = taskValue || "New Task";
 
-    
+
     // Edit Task
     taskContent.addEventListener('dblclick', () => {
-        taskContent.contentEditable = true; 
-        taskContent.focus(); 
-        taskContent.classList.add('editable'); 
-        
-        taskDiv.setAttribute("draggable", "false"); 
+        taskContent.contentEditable = true;
+        taskContent.focus();
+        taskContent.classList.add('editable');
+
+        taskDiv.setAttribute("draggable", "false");
     });
 
     taskContent.addEventListener('blur', () => {
         taskContent.contentEditable = false;
         taskContent.classList.remove('editable');
-        taskDiv.setAttribute("draggable", "true"); 
+        taskDiv.setAttribute("draggable", "true");
 
         if (taskContent.textContent.trim() === "") {
-             taskDiv.remove(); 
-             // taskContent.textContent = "Empty Task"; // Default text when empty
+            taskDiv.remove();
+            // taskContent.textContent = "Empty Task"; // Default text when empty
         }
     });
 
     taskContent.addEventListener('keydown', (e) => {
         if (e.key === 'Enter') {
-            e.preventDefault(); 
-            taskContent.blur(); 
+            e.preventDefault();
+            taskContent.blur();
         }
     });
 
-    
+
     // Delete button
     const deleteBtn = document.createElement('span');
     deleteBtn.className = "close-btn";
@@ -130,51 +149,65 @@ function createTask(text) {
 }
 
 
-// Función para cambiar estilos
+// Function to change styles
 function changeStyle(chosenStyle) {
-  const body = document.body;
-  // Puedes limpiar estilos previos o agregar clases
-  body.className = ''; // Limpia clases previas
-  // Asignar diferentes clases o estilos
-  switch(chosenStyle) {
-    case 'lovely':
-      body.classList.add('lovely-style');
-      break;
-    case 'sunset':
-      body.classList.add('sunset-style');
-      break;
-    case 'night':
-      body.classList.add('night-style');
-      break;
-    case 'plant':
-      body.classList.add('plant-style');
-      break;
-  }
+    const body = document.body;
+    body.className = ''; // Limpia clases previas
+    switch (chosenStyle) {
+        case 'lovely':
+            body.classList.add('lovely-style');
+            break;
+        case 'sunset':
+            body.classList.add('sunset-style');
+            break;
+        case 'night':
+            body.classList.add('night-style');
+            break;
+        case 'plant':
+            body.classList.add('plant-style');
+            break;
+    }
 }
 
 function updateClockNav() {
-  const now = new Date();
-  const optionsDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const optionsHour = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const now = new Date();
+    const optionsDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const optionsHour = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
 
-  const date = now.toLocaleDateString('en-EN', optionsDate);
-  const hour = now.toLocaleTimeString('en-EN', optionsHour);
+    const date = now.toLocaleDateString('en-EN', optionsDate);
+    const hour = now.toLocaleTimeString('en-EN', optionsHour);
 
-  const clockNav = document.getElementById('nav-clock');
-  if (!clockNav) return;
+    const clockNav = document.getElementById('nav-clock');
+    if (!clockNav) return;
 
-  clockNav.textContent = `${date} - ${hour}`;
+    clockNav.textContent = `${date} - ${hour}`;
 }
 
-		
-		/*
-		DRAGOVER
-		Evento DRAGOVER: Cuando un elemento arrastrable pasa sobre este panel o elemnto, se ejecuta
-		la función allowDrop. IMPORTANTE PQ por defecto, los navegadores no lo permiten. 	
-		
-		DROP
-		Evento DROP. Al sueltar un elemento arrastrable sobre el panel/elemento, se ejecuta 
-		la función drop. Esta función mueve el elemento arrastrado (la tarea o lo que sea) dentro 
-		del panel o elemento donde se soltó.		
+//search between existing tasks in the document
+function filterTasks() {
+    const filterInput = document.getElementById('filterSearch');
+    const textSearch = filterInput.value.toLowerCase().trim();
+    const allTheTasks = document.querySelectorAll('.single-task');
 
-		*/
+    allTheTasks.forEach(task => {
+        const taskTitle = task.querySelector('p').textContent.toLowerCase();
+        if (taskTitle.includes(textSearch)) {
+            task.style.display = 'flex';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+}
+
+
+/*
+DRAGOVER
+Evento DRAGOVER: Cuando un elemento arrastrable pasa sobre este panel o elemnto, se ejecuta
+la función allowDrop. IMPORTANTE PQ por defecto, los navegadores no lo permiten. 	
+	
+DROP
+Evento DROP. Al sueltar un elemento arrastrable sobre el panel/elemento, se ejecuta 
+la función drop. Esta función mueve el elemento arrastrado (la tarea o lo que sea) dentro 
+del panel o elemento donde se soltó.		
+
+*/
